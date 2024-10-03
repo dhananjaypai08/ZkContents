8236ce56dadcac81071dcf47c35d8cc39323b951
dhananjaypai08 <dhananjay2002pai@gmail.com>
2024-10-03 10:54:08 +0530

    Initialize subgraph

---
+++ zkcdn/zkcdnGraph/src/zk-cdn.ts
@@ -0,0 +1,119 @@
+import {
+  Approval as ApprovalEvent,
+  ApprovalForAll as ApprovalForAllEvent,
+  ConvertedString as ConvertedStringEvent,
+  Encrypted_CID as Encrypted_CIDEvent,
+  Mint as MintEvent,
+  OwnershipTransferred as OwnershipTransferredEvent,
+  Transfer as TransferEvent
+} from "../generated/ZkCDN/ZkCDN"
+import {
+  Approval,
+  ApprovalForAll,
+  ConvertedString,
+  Encrypted_CID,
+  Mint,
+  OwnershipTransferred,
+  Transfer
+} from "../generated/schema"
+
+export function handleApproval(event: ApprovalEvent): void {
+  let entity = new Approval(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.approved = event.params.approved
+  entity.tokenId = event.params.tokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleApprovalForAll(event: ApprovalForAllEvent): void {
+  let entity = new ApprovalForAll(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.operator = event.params.operator
+  entity.approved = event.params.approved
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleConvertedString(event: ConvertedStringEvent): void {
+  let entity = new ConvertedString(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.encrypted_cid = event.params.encrypted_cid
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleEncrypted_CID(event: Encrypted_CIDEvent): void {
+  let entity = new Encrypted_CID(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.encrypted_cid = event.params.encrypted_cid
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleMint(event: MintEvent): void {
+  let entity = new Mint(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.to = event.params.to
+  entity.encrypted_cid = event.params.encrypted_cid
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  let entity = new Transfer(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.tokenId = event.params.tokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
---
+++ zkcdn/zkcdnGraph/tests/zk-cdn-utils.ts
@@ -0,0 +1,156 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  Approval,
+  ApprovalForAll,
+  ConvertedString,
+  Encrypted_CID,
+  Mint,
+  OwnershipTransferred,
+  Transfer
+} from "../generated/ZkCDN/ZkCDN"
+
+export function createApprovalEvent(
+  owner: Address,
+  approved: Address,
+  tokenId: BigInt
+): Approval {
+  let approvalEvent = changetype<Approval>(newMockEvent())
+
+  approvalEvent.parameters = new Array()
+
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("approved", ethereum.Value.fromAddress(approved))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return approvalEvent
+}
+
+export function createApprovalForAllEvent(
+  owner: Address,
+  operator: Address,
+  approved: boolean
+): ApprovalForAll {
+  let approvalForAllEvent = changetype<ApprovalForAll>(newMockEvent())
+
+  approvalForAllEvent.parameters = new Array()
+
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("operator", ethereum.Value.fromAddress(operator))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("approved", ethereum.Value.fromBoolean(approved))
+  )
+
+  return approvalForAllEvent
+}
+
+export function createConvertedStringEvent(
+  encrypted_cid: BigInt
+): ConvertedString {
+  let convertedStringEvent = changetype<ConvertedString>(newMockEvent())
+
+  convertedStringEvent.parameters = new Array()
+
+  convertedStringEvent.parameters.push(
+    new ethereum.EventParam(
+      "encrypted_cid",
+      ethereum.Value.fromUnsignedBigInt(encrypted_cid)
+    )
+  )
+
+  return convertedStringEvent
+}
+
+export function createEncrypted_CIDEvent(encrypted_cid: BigInt): Encrypted_CID {
+  let encryptedCidEvent = changetype<Encrypted_CID>(newMockEvent())
+
+  encryptedCidEvent.parameters = new Array()
+
+  encryptedCidEvent.parameters.push(
+    new ethereum.EventParam(
+      "encrypted_cid",
+      ethereum.Value.fromUnsignedBigInt(encrypted_cid)
+    )
+  )
+
+  return encryptedCidEvent
+}
+
+export function createMintEvent(to: Address, encrypted_cid: BigInt): Mint {
+  let mintEvent = changetype<Mint>(newMockEvent())
+
+  mintEvent.parameters = new Array()
+
+  mintEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  mintEvent.parameters.push(
+    new ethereum.EventParam(
+      "encrypted_cid",
+      ethereum.Value.fromUnsignedBigInt(encrypted_cid)
+    )
+  )
+
+  return mintEvent
+}
+
+export function createOwnershipTransferredEvent(
+  previousOwner: Address,
+  newOwner: Address
+): OwnershipTransferred {
+  let ownershipTransferredEvent = changetype<OwnershipTransferred>(
+    newMockEvent()
+  )
+
+  ownershipTransferredEvent.parameters = new Array()
+
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousOwner",
+      ethereum.Value.fromAddress(previousOwner)
+    )
+  )
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
+  )
+
+  return ownershipTransferredEvent
+}
+
+export function createTransferEvent(
+  from: Address,
+  to: Address,
+  tokenId: BigInt
+): Transfer {
+  let transferEvent = changetype<Transfer>(newMockEvent())
+
+  transferEvent.parameters = new Array()
+
+  transferEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return transferEvent
+}
